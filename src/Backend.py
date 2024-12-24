9d33d99d3f3ccf9f946c72b7356a983810aae489
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-12-25 02:55:19 +0300

    Show Country code and Country name together in gui

--- src/Backend.py
+++ src/Backend.py
@@ -6,10 +6,15 @@ from src.Translator import Translator
 bigQueryConnector = BigQueryConnector()
 queryGenerator = QueryGenerotor()
 
+f = open("src/CodesDicts/CAMEO.country_codes2text.py", "r")
+countries_code2name = eval(f.read())
+f.close()
+
 get_countries_query = queryGenerator.generate_get_country_query()
 countries_code = bigQueryConnector.get_data(get_countries_query)
 countries_code = countries_code.dropna().reset_index(drop=True)
 countries_code = sorted(countries_code['CountryCode'].tolist())
+countries_code = [country+" : "+countries_code2name[country] if country in countries_code2name else country for country in countries_code]
 
 def download_data(contry_1, contry_2, start_date, end_date, limit, path):
     query = queryGenerator.generate_query(contry_1, contry_2, start_date, end_date, limit)
